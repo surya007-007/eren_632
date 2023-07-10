29e4dee0070a49b3840093e5dafa798b036bf8e5
surya007-007 <190020042@iitdh.ac.in>
2023-07-10 10:49:23 +0530

    Eren motto

--- mikasa.java
+++ mikasa.java
@@ -3,6 +3,7 @@ public class mikasa
     public static void main(String a[])
     {
         System.out.println("Eat laddu when hungry");
+        System.out.println("I shall keep moving forward");
         System.out.println("After eating fight the evil");
         
     }
